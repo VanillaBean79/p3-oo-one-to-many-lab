717af476a9fb67b78782bc13f4af6dfdbb3d12b9
Ruben Salazar <rubensalazar@rocketmail.com>
2025-01-17 14:04:08 -0600

    one to many lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,39 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+
+        if pet_type in Pet.PET_TYPES:
+            self.pet_type = pet_type
+        else:
+            raise Exception(f"Invalid pet type '{pet_type}'. Valid pet types are {Pet.PET_TYPES}.")
+
+        if owner:
+            owner.add_pet(self)
+
+        Pet.all.append(self)
+        
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.pets_list = []
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise TypeError("Only objects of type 'Pet' can be added.")
+        pet.owner = self
+        if pet not in self.pets_list:
+            self.pets_list.append(pet)
+
+
+    def pets(self):
+        return self.pets_list
+
+    def get_sorted_pets(self):
+        return sorted(self.pets_list, key=lambda pet: pet.name)
