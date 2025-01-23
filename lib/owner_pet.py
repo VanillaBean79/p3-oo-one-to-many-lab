dfdad6100236d039ce7bbd656fc00a06b2f0c1a2
Ruben Salazar <rubensalazar@rocketmail.com>
2025-01-23 15:58:19 -0600

    one to many lab (refractor)

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,39 +1,39 @@
-class Pet:
-
-    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
-    all = []
-
-    def __init__(self, name, pet_type, owner=None):
-        self.name = name
-        self.pet_type = pet_type
-        self.owner = owner
-
-        if pet_type in Pet.PET_TYPES:
-            self.pet_type = pet_type
-        else:
-            raise Exception(f"Invalid pet type '{pet_type}'. Valid pet types are {Pet.PET_TYPES}.")
-
-        if owner:
-            owner.add_pet(self)
-
-        Pet.all.append(self)
-        
-
 class Owner:
     def __init__(self, name):
         self.name = name
         self.pets_list = []
 
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
     def add_pet(self, pet):
         if not isinstance(pet, Pet):
-            raise TypeError("Only objects of type 'Pet' can be added.")
-        pet.owner = self
+            raise Exception("Pet must be in list of pets.")
         if pet not in self.pets_list:
             self.pets_list.append(pet)
 
-
-    def pets(self):
-        return self.pets_list
+        pet.owner = self
 
     def get_sorted_pets(self):
-        return sorted(self.pets_list, key=lambda pet: pet.name)
+        return sorted(self.pets(), key=lambda pet: pet.name)
+        
+
+class Pet:
+
+    PET_TYPES = ["dog","cat","rodent","bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner 
+        
+        self.pet_type = pet_type if pet_type in Pet.PET_TYPES else None
+        if self.pet_type is None:
+            raise Exception("Pet must be on the list.")
+
+        Pet.all.append(self)
+        
+       
+
+       
